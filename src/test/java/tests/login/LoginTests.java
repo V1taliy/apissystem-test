2054a83018a56426f1aa18ab08172b62f1faaefe
Alexander Bakhin <AlexanderB@migfin.com>
2016-07-21 16:35:06 +0300

    fixed login tests

--- src/test/java/tests/login/LoginTests.java
+++ src/test/java/tests/login/LoginTests.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 import tests.Fixture;
 import utils.PropertyLoader;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginTests extends Fixture {
 
     private static final Logger log = Logger.getLogger(LoginTests.class);
@@ -15,6 +17,7 @@ public class LoginTests extends Fixture {
     private static final String USER_PASSWORD = PropertyLoader.loadProperty("user.password");
     private static final String ADMIN_NAME = PropertyLoader.loadProperty("admin.name");
     private static final String ADMIN_PASSWORD = PropertyLoader.loadProperty("admin.password");
+    private static final int newImpWait = 300;
 
     @Test(priority = 1)
     public void openWebSite() {
@@ -22,24 +25,19 @@ public class LoginTests extends Fixture {
         apisSystem.loginPage.waitInvisibilityLoader();
         Assert.assertEquals(apisSystem.loginPage.getPageURL(),
                 "http://development.apis.office.dev/manage/login");
+//        impWait = PropertyLoader.loadProperty("wait.timeout1sec");
+        impWait = String.valueOf(newImpWait);
+        driverWrapper.manage().timeouts().implicitlyWait(Long.parseLong(impWait), TimeUnit.MILLISECONDS);
     }
 
     @Test(priority = 2, dependsOnMethods = {"openWebSite"})
     public void negativeLogin() {
-        apisSystem.loginPage.inputUserName(FAKE_NAME);
-        apisSystem.loginPage.inputPassword(FAKE_PASSWORD);
-        apisSystem.loginPage.clickLoginButton();
-        apisSystem.loginPage.waitAlertInvalidUserOrPass();
-        Assert.assertTrue(apisSystem.loginPage.isAlertInvalidUserOrPassPresent());
+        fakeLoginTestCase(FAKE_NAME, FAKE_PASSWORD);
     }
 
     @Test(priority = 3, dependsOnMethods = {"openWebSite"})
     public void negativeLogin2() {
-        apisSystem.loginPage.inputUserName(FAKE_NAME + 1);
-        apisSystem.loginPage.inputPassword(FAKE_PASSWORD + 1);
-        apisSystem.loginPage.clickLoginButton();
-        apisSystem.loginPage.waitAlertInvalidUserOrPass();
-        Assert.assertTrue(apisSystem.loginPage.isAlertInvalidUserOrPassPresent());
+        fakeLoginTestCase(FAKE_NAME + "test", FAKE_PASSWORD + "test");
     }
 
     @Test(priority = 4, dependsOnMethods = {"openWebSite"})
@@ -53,6 +51,7 @@ public class LoginTests extends Fixture {
         apisSystem.loginPage.inputUserName(USER_NAME);
         apisSystem.loginPage.inputPassword(USER_PASSWORD);
         apisSystem.loginPage.clickLoginButton();
+        apisSystem.loginPage.waitInvisibilityPanelBody();
         Assert.assertTrue(apisSystem.mainPage.isWelcomeToApisSystemPresent());
         apisSystem.mainPage.clickLogoutButton();
     }
@@ -62,8 +61,18 @@ public class LoginTests extends Fixture {
         apisSystem.loginPage.inputUserName(ADMIN_NAME);
         apisSystem.loginPage.inputPassword(ADMIN_PASSWORD);
         apisSystem.loginPage.clickLoginButton();
+        apisSystem.loginPage.waitInvisibilityPanelBody();
         Assert.assertTrue(apisSystem.mainPage.isWelcomeToApisSystemPresent());
         apisSystem.mainPage.clickLogoutButton();
+    }
+
+    private void fakeLoginTestCase(String fakeName, String fakePassword) {
+        apisSystem.loginPage.inputUserName(fakeName);
+        apisSystem.loginPage.inputPassword(fakePassword);
+        apisSystem.loginPage.clickLoginButton();
+        apisSystem.loginPage.waitInvisibilityPanelBody();
+        apisSystem.loginPage.waitAlertInvalidUserOrPass();
+        Assert.assertTrue(apisSystem.loginPage.isAlertInvalidUserOrPassPresent());
     }
 
 }
