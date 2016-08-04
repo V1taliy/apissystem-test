c661cc2f1c823395b62ff16ff1a58379cf60c978
Alexander Bakhin <AlexanderB@migfin.com>
2016-08-04 11:24:30 +0300

    created DeskExpirationTime package, class and xml file

---
+++ src/test/java/tests/navigation/deskExpirationTime/DeskExpirationTime.java
@@ -0,0 +1,15 @@
+package tests.navigation.deskExpirationTime;
+
+import org.apache.log4j.Logger;
+import pages.Page;
+import utils.WebDriverWrapper;
+
+public class DeskExpirationTime extends Page {
+
+    private static final Logger log = Logger.getLogger(DeskExpirationTime.class);
+
+    public DeskExpirationTime(WebDriverWrapper driverWrapper) {
+        super(driverWrapper);
+    }
+
+}
