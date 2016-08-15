eb59337a69a27deb5bde057b7135e405b492b8bf
Alexander Bakhin <AlexanderB@migfin.com>
2016-08-15 11:28:04 +0300

    changed Desk exp.time test class

--- src/test/java/tests/navigation/deskExpirationTime/DeskExpirationTimeTest.java
+++ src/test/java/tests/navigation/deskExpirationTime/DeskExpirationTimeTest.java
@@ -49,7 +49,7 @@ public class DeskExpirationTimeTest extends Fixture {
         }
     }
 
-    @Test(priority = 4, dependsOnMethods = {"sortTabs"})
+    @Test(priority = 4, dependsOnMethods = {"goToDeskExpirationTime"})
     public void clickNavigationButtonsOnList() {
         for (int i = 0; i <= BUTTONS_NAME_ARRAY.length - 1; i++) {
             if (apisSystem.deskExpirationTime.isLoadedClassHaveAttributeInClass()) {
@@ -59,7 +59,7 @@ public class DeskExpirationTimeTest extends Fixture {
         }
     }
 
-    @Test(priority = 5, dependsOnMethods = {"clickNavigationButtonsOnList"})
+    @Test(priority = 5, dependsOnMethods = {"goToDeskExpirationTime"})
     public void clickNavigationIndexButtonsOnList() {
         for (int i = 0; i <= 3; i++) {
             if (apisSystem.deskExpirationTime.isLoadedClassHaveAttributeInClass()) {
@@ -70,13 +70,13 @@ public class DeskExpirationTimeTest extends Fixture {
         apisSystem.deskExpirationTime.scrollPageToNavigationWrapper();
     }
 
-    @Test(priority = 6, dependsOnMethods = {"clickNavigationIndexButtonsOnList"})
+    @Test(priority = 6, dependsOnMethods = {"goToDeskExpirationTime"})
     public void clickCheckbox1() {
         apisSystem.deskExpirationTime.waitLoadedAttributeToBeEmptyClass();
         clickCheckbox(1);
     }
 
-    @Test(priority = 7, dependsOnMethods = {"clickCheckbox1"})
+    @Test(priority = 7, dependsOnMethods = {"goToDeskExpirationTime"})
     public void popupEditInputValueClickCancelButton() {
         apisSystem.edit.inputExpirationTime(11);
         apisSystem.edit.clickButtonSaveOrCancel(false);
@@ -84,12 +84,12 @@ public class DeskExpirationTimeTest extends Fixture {
         Assert.assertEquals(apisSystem.deskExpirationTime.getExpirationTimeValue(1), 10);
     }
 
-    @Test(priority = 8, dependsOnMethods = {"popupEditInputValueClickCancelButton"})
+    @Test(priority = 8, dependsOnMethods = {"goToDeskExpirationTime"})
     public void clickCheckbox2() {
         clickCheckbox(2);
     }
 
-    @Test(priority = 9, dependsOnMethods = {"clickCheckbox2"})
+    @Test(priority = 9, dependsOnMethods = {"goToDeskExpirationTime"})
     public void popupEditInputValueClickSaveButton() {
         apisSystem.edit.inputExpirationTime(100);
         apisSystem.edit.clickButtonSaveOrCancel(true);
@@ -98,13 +98,13 @@ public class DeskExpirationTimeTest extends Fixture {
         Assert.assertTrue(apisSystem.deskExpirationTime.isMessageSuccessPresent());
     }
 
-    @Test(priority = 10, dependsOnMethods = {"popupEditInputValueClickSaveButton"})
+    @Test(priority = 10, dependsOnMethods = {"goToDeskExpirationTime"})
     public void switchToBrands() {
         apisSystem.mainPage.clickOnNavigationItem(2);
         Assert.assertEquals(apisSystem.deskExpirationTime.getCurrentPageURL(), BRANDS_URL);
     }
 
-    @Test(priority = 11, dependsOnMethods = {"switchToBrands"})
+    @Test(priority = 11, dependsOnMethods = {"goToDeskExpirationTime"})
     public void changeSomeBrandsEnabled() {
         apisSystem.brandsPage.waitLoadedAttributeToBeEmptyClass();
         apisSystem.brandsPage.selectTableSort(2);
@@ -116,13 +116,13 @@ public class DeskExpirationTimeTest extends Fixture {
         Assert.assertTrue(apisSystem.brandsPage.isMessageSuccessPresent());
     }
 
-    @Test(priority = 12, dependsOnMethods = {"changeSomeBrandsEnabled"})
+    @Test(priority = 12, dependsOnMethods = {"goToDeskExpirationTime"})
     public void switchToDeskExpTime() {
         apisSystem.mainPage.clickOnNavigationItem(5);
         Assert.assertEquals(apisSystem.deskExpirationTime.getCurrentPageURL(), DESK_EXPIRATION_TIME_URL);
     }
 
-    @Test(priority = 13, dependsOnMethods = {"switchToDeskExpTime"})
+    @Test(priority = 13, dependsOnMethods = {"goToDeskExpirationTime"})
     public void selectBrand() {
         apisSystem.deskExpirationTime.filterClickedOnSelectBrand();
         apisSystem.deskExpirationTime.filterClickOnSearchOrReset(true);
@@ -132,7 +132,7 @@ public class DeskExpirationTimeTest extends Fixture {
     }
 
     // TODO for correctly work, must be fixed bug APIS-185
-    @Test(priority = 14, dependsOnMethods = {"selectBrand"})
+    @Test(priority = 14, dependsOnMethods = {"goToDeskExpirationTime"})
     public void selectDesk() {
         apisSystem.deskExpirationTime.filterClickOnDesk();
         apisSystem.deskExpirationTime.filterClikedOnDesk();
@@ -140,7 +140,7 @@ public class DeskExpirationTimeTest extends Fixture {
         apisSystem.deskExpirationTime.waitLoadedAttributeToBeEmptyClass();
     }
 
-    @Test(priority = 15, dependsOnMethods = {"selectDesk"})
+    @Test(priority = 15, dependsOnMethods = {"goToDeskExpirationTime"})
     public void clickResetButton() {
         String expectedResult = "Select";
         apisSystem.deskExpirationTime.filterClickOnSearchOrReset(false);
@@ -149,7 +149,7 @@ public class DeskExpirationTimeTest extends Fixture {
         Assert.assertEquals(apisSystem.deskExpirationTime.filterGetBrandValue(), expectedResult);
     }
 
-    @Test(priority = 16, dependsOnMethods = {"clickResetButton"})
+    @Test(priority = 16, dependsOnMethods = {"goToDeskExpirationTime"})
     public void selectBrandAndDesk() {
         apisSystem.deskExpirationTime.filterClickedOnSelectBrand();
         apisSystem.deskExpirationTime.waitDeskToBeActive();
