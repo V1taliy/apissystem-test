37b58c434e55723cc9a5973b1954a25046098089
Alexander Bakhin <AlexanderB@migfin.com>
2016-08-22 12:20:46 +0300

    created EditDesks class for popup

---
+++ src/main/java/pages/users/popup/EditDesks.java
@@ -0,0 +1,12 @@
+package pages.users.popup;
+
+import pages.Page;
+import utils.WebDriverWrapper;
+
+public class EditDesks extends Page {
+
+    public EditDesks(WebDriverWrapper driverWrapper) {
+        super(driverWrapper);
+    }
+
+}
