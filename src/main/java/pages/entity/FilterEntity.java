aa97c739e7699002455cd0f091ea0dc6ce0eb01a
Alexander Bakhin <AlexanderB@migfin.com>
2016-08-29 13:33:08 +0300

    change FilterEntity class

--- src/main/java/pages/entity/FilterEntity.java
+++ src/main/java/pages/entity/FilterEntity.java
@@ -1,8 +1,14 @@
 package pages.entity;
 
 import org.apache.log4j.Logger;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.Page;
+import utils.PropertyLoader;
 import utils.WebDriverWrapper;
+
+import java.util.List;
 
 public class FilterEntity extends Page {
 
@@ -10,6 +16,42 @@ public class FilterEntity extends Page {
 
     public FilterEntity(WebDriverWrapper driverWrapper) {
         super(driverWrapper);
+    }
+
+    /**
+     * Click on button 'Search' or 'Reset' on entity 'Filter'
+     *
+     * @param button where
+     *               true - Search
+     *               false - Reset
+     */
+    public void clickSearchOrResetButton(boolean button) {
+        List<WebElement> buttonsList = web.getElements("buttonsList");
+        if (button) {
+            buttonsList.get(0).click();
+        } else {
+            buttonsList.get(1).click();
+        }
+    }
+
+    /**
+     * Wait for processing disappear
+     */
+    public void waitLoadedAttributeToBeEmptyClass() {
+        WebElement element = web.getElement("isLoadedElement");
+        WebDriverWait wait = new WebDriverWait(driverWrapper.getOriginalDriver(),
+                Long.parseLong(PropertyLoader.loadProperty("wait.timeout5sec")));
+        wait.until(ExpectedConditions.attributeToBe(element, "class", ""));
+    }
+
+    /**
+     * Check is processing element present on the page
+     *
+     * @return true if processing element present, otherwise false
+     */
+    public boolean isLoadedClassHaveAttributeInClass() {
+        WebElement element = web.getElement("isLoadedElement");
+        return element.getAttribute("class").contains("traditional");
     }
 
 }
