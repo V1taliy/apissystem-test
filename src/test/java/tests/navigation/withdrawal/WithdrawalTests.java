21421da0ba4e200c45523d6adeccfac341402845
Alexander Bakhin <AlexanderB@migfin.com>
2016-09-07 18:49:01 +0300

    added test method for switch Brands page change some logic in LoginTests class

--- src/test/java/tests/navigation/withdrawal/WithdrawalTests.java
+++ src/test/java/tests/navigation/withdrawal/WithdrawalTests.java
@@ -1,11 +1,16 @@
 package tests.navigation.withdrawal;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import tests.Fixture;
 import tests.login.LoginTests;
 import tests.login.TestUserLogin;
+import tests.navigation.brands.BrandsTests;
+import utils.PropertyLoader;
 
 public class WithdrawalTests extends Fixture {
+
+    private static final String BRANDS_URL = PropertyLoader.loadProperty("brands.url");
 
     @Test(priority = 1)
     public void testUsersLogin() {
@@ -14,9 +19,13 @@ public class WithdrawalTests extends Fixture {
 
     @Test(priority = 2)
     public void adminLogin() {
-        LoginTests.adminLogin();
+        LoginTests.loginForAdmin(false);
     }
 
-
+    @Test(priority = 3)
+    public void switchToBrands() {
+        apisSystem.mainPage.clickOnNavigationItem(2);
+        Assert.assertEquals(apisSystem.brandsPage.getCurrentPageURL(), BRANDS_URL);
+    }
 
 }
