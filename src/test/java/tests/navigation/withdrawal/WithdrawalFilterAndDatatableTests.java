6342b18c6b28db44f77d881f272aada49ac69fd5
Alexander Bakhin <AlexanderB@migfin.com>
2016-09-21 11:52:48 +0300

    rename WithdrawalTests to WithdrawalDeclineTests and create test class for Withdrawal filter and datatable tests

---
+++ src/test/java/tests/navigation/withdrawal/WithdrawalFilterAndDatatableTests.java
@@ -0,0 +1,10 @@
+package tests.navigation.withdrawal;
+
+import org.apache.log4j.Logger;
+import tests.Fixture;
+
+public class WithdrawalFilterAndDatatableTests extends Fixture {
+
+    private static final Logger log = Logger.getLogger(WithdrawalFilterAndDatatableTests.class);
+
+}
