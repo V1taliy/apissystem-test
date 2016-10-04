4d6f8fca7a096d6cbe416dadfed9dde1999b9800
Alexander Bakhin <AlexanderB@migfin.com>
2016-10-04 13:04:58 +0300

    relocated waits from filter entity to list entity

--- src/main/java/pages/entity/ListEntity.java
+++ src/main/java/pages/entity/ListEntity.java
@@ -2,7 +2,10 @@ package pages.entity;
 
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.Page;
+import utils.PropertyLoader;
 import utils.WebDriverWrapper;
 
 import java.util.List;
@@ -51,6 +54,26 @@ public class ListEntity extends Page {
             }
         }
         return -1;
+    }
+
+    /**
+     * Wait for processing disappear
+     */
+    public void waitLoadedAttributeToBeEmptyClass() {
+        WebElement element = web.getElement("isLoadedElement");
+        WebDriverWait wait = new WebDriverWait(driverWrapper.getOriginalDriver(),
+                Long.parseLong(PropertyLoader.loadProperty("wait.timeout40sec")));
+        wait.until(ExpectedConditions.attributeToBe(element, "class", ""));
+    }
+
+    /**
+     * Check is processing element present on the page
+     *
+     * @return true if processing element present, otherwise false
+     */
+    public boolean isLoadedClassHaveAttributeInClass() {
+        WebElement element = web.getElement("isLoadedElement");
+        return element.getAttribute("class").contains("traditional");
     }
 
 }
